a08f935bfeb613fd63e145f887a4a712c7eba1af
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-13 23:55:11 +0100

    variable names updated

--- Trainings/factorial.sh
+++ Trainings/factorial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-counter=$1	#bash factorial.sh 6(number to run)
-factorial=1
-while [ $counter -gt 0 ]
+COUNTER=$1	#bash factorial.sh 6(number to run)
+FACTORIAL=1
+while [ $COUNTER -gt 0 ]
 do
-factorial=$(( $factorial * $counter ))
-counter=$(( $counter - 1 ))
+FACTORIAL=$(( $FACTORIAL * $COUNTER ))
+COUNTER=$(( $COUNTER - 1 ))
 done
-echo $factorial
+echo $FACTORIAL
